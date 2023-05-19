9ee8df0081aa75058d72b0045f05b29f4a42b7c7
nzrr <bonuzx1@gmail.com>
2023-05-19 11:07:07 +0000

    fixing the swipe to be full screen and add image

--- lib/swipe_home.dart
+++ lib/swipe_home.dart
@@ -2,28 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:appinio_swiper/appinio_swiper.dart';
 
 class SwipeHome extends StatefulWidget {
-  const SwipeHome({super.key});
 
   @override
   _SwipeHomeState createState() => _SwipeHomeState();
 }
 
 class _SwipeHomeState extends State<SwipeHome> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     List<String> pages = ['Page 1', 'Page 2', 'Page 3'];
-    int currentIndex = 0;
+    final orientation = MediaQuery.of(context).orientation;
+    BoxFit fit;
+    if (orientation == Orientation.landscape) {
+      fit = BoxFit.fitWidth;
+    } else {
+      fit = BoxFit.fitHeight;
+    }
     return Scaffold(
       body: AppinioSwiper(
         cardsBuilder: (BuildContext context, int index) {
-          return Container(
-            alignment: Alignment.center,
-            color: Colors.blueAccent,
-            child: Text(index.toString()),
+          return SizedBox(
+            height: screenHeight,
+            width: screenWidth,
+            child: ClipRRect(
+              child: Image.asset(
+                'assets/image/unsplash-kitsune-4.jpg',
+                fit: fit,
+              ),
+            ),
           );
         },
         cardsCount: 20,
+        onSwipe: (index, direction) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
       ),
     );
   }
+
 }
